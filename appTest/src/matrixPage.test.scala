4d4ab842907303adb0886737d77ea6c3e79118ea
Simon Parten <quafadas@gmail.com>
2026-03-30 08:57:14 +0200

    .

--- appTest/src/matrixPage.test.scala
+++ appTest/src/matrixPage.test.scala
@@ -5,14 +5,22 @@ import com.microsoft.playwright.*
 class MatrixPageTest extends PlaywrightTestBase:
 
   override def beforeAll(): Unit =
+    startServer()
     pw = Playwright.create()
-    browser = pw.firefox().launch(options);
-    page = browser.newPage();
+    browser = pw.firefox().launch(options)
+    page = browser.newPage()
     page.setDefaultTimeout(30000)
   end beforeAll
 
+  override def afterAll(): Unit =
+    if page != null then page.close()
+    if browser != null then browser.close()
+    if pw != null then pw.close()
+    stopServer()
+  end afterAll
 
-  test("placeholder test") {
-
-    assertEquals(1 + 1, 2)
+  test("home page loads") {
+    page.navigate(baseUrl)
+    val title = page.title()
+    assert(title.nonEmpty, s"Expected a non-empty page title, got: '$title'")
   }
--- appTest/src/pwTest.scala
+++ appTest/src/pwTest.scala
@@ -1,6 +1,9 @@
 package mathlify
 
 import com.microsoft.playwright.*
+import com.sun.net.httpserver.{HttpServer, HttpExchange}
+import java.net.InetSocketAddress
+import java.nio.file.{Files, Path}
 import scala.compiletime.uninitialized
 import scala.collection.JavaConverters.seqAsJavaListConverter
 
@@ -8,7 +11,48 @@ trait PlaywrightTestBase extends munit.FunSuite:
   var pw: Playwright = uninitialized
   var browser: Browser = uninitialized
   var page: Page = uninitialized
+  var server: HttpServer = uninitialized
+  var baseUrl: String = uninitialized
 
   val options = new BrowserType.LaunchOptions()
     .setHeadless(true)
     .setArgs(List("--no-sandbox").asJava)
+
+  private val contentTypes = Map(
+    "html" -> "text/html",
+    "js" -> "application/javascript",
+    "css" -> "text/css",
+    "map" -> "application/json"
+  )
+
+  protected def startServer(): Unit =
+    val publishDir = Path.of(BuildInfo.publishDir)
+    server = HttpServer.create(InetSocketAddress(0), 0)
+    val port = server.getAddress.getPort
+    baseUrl = s"http://localhost:$port"
+
+    server.createContext("/", (exchange: HttpExchange) => {
+      val requestPath = exchange.getRequestURI.getPath.stripPrefix("/")
+      val filePath = if requestPath.isEmpty then "index.html" else requestPath
+      val resolved = publishDir.resolve(filePath).normalize()
+
+      if !resolved.startsWith(publishDir) then
+        exchange.sendResponseHeaders(403, -1)
+        exchange.close()
+      else if Files.exists(resolved) && !Files.isDirectory(resolved) then
+        val bytes = Files.readAllBytes(resolved)
+        val ext = filePath.split('.').lastOption.getOrElse("")
+        val ct = contentTypes.getOrElse(ext, "application/octet-stream")
+        exchange.getResponseHeaders.add("Content-Type", ct)
+        exchange.sendResponseHeaders(200, bytes.length.toLong)
+        exchange.getResponseBody.write(bytes)
+        exchange.getResponseBody.close()
+      else
+        exchange.sendResponseHeaders(404, -1)
+        exchange.close()
+    })
+    server.start()
+  end startServer
+
+  protected def stopServer(): Unit =
+    if server != null then server.stop(0)
